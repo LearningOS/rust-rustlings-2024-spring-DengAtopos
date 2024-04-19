3e48af38af17749eefa18955cbcc4d61502cd515
DengAtopos <166409320+DengAtopos@users.noreply.github.com>
2024-04-19 02:09:47 +0000

    sixth

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let data = "Rust is great!".to_string();
@@ -22,7 +21,7 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: String) {
-    data = &data.to_uppercase();
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
